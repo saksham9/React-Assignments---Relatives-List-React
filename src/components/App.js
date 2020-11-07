663c96b6c3292a39dd618682b3a76d6a573e62ad
saksham9 <agrawal.saksham9@gmail.com>
2020-11-07 11:36:49 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  render() {
+    let relatives = ["saksham", "rohit", "ayushi", "mdks"];
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+    return (
+      <div id="main">
+        {/* Do not remove the main div */}
+        <ol key="relativeList">
+          {relatives.map((relative, index) => (
+            <li key={`relativeListItem${index + 1}`}>{relative}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
